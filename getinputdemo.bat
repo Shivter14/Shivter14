56839a6a01ce46ade1ad380a05074e212861b141
Shivter <98386496+Shivter14@users.noreply.github.com>
2024-09-02 20:38:24 +0200

    Update getinputdemo.bat
    Added scrolling information
    (+bugfix)

--- getinputdemo.bat
+++ getinputdemo.bat
@@ -1,5 +1,5 @@
-@echo off
-setlocal enabledelayedexpansion
+@echo off & setlocal enabledelayedexpansion
+cd "%~dp0"
 REM == \e definition ==
 for /f %%a in ('echo prompt $E^| cmd') do set "\e=%%a"
 
@@ -61,5 +61,7 @@ for /f "tokens=1-4 delims=:.," %%a in ("!time: =0!") do set /a "t2=(((1%%a*60)+1
 		if "!highlited!"=="False" echo=%\e%[!y!;!x!H%\e%[38;2;0;0;0;48;2;127;127;127m%\e%7            %\e%8%\e%[B   Button   %\e%8%\e%[2B            %\e%[H
 	)
 	Rem Displaying keyboard and mouse information
-	set /p "=%\e%[10;4H%\e%[48;2;255;255;255;38;2;0;0;0m%\e%[2KX: !mouseXpos! Y: !mouseYpos! | !keysPressed!"
+	if !mouseXpos! gtr 0 if !mouseXpos! leq !this.modeW! if !mouseYpos! gtr 0 if !mouseYpos! leq !this.modeH! set /a scrollpos+=wheelDelta
+	set wheelDelta=0
+	set /p "=%\e%[48;2;255;255;255;38;2;0;0;0m%\e%[10;4H%\e%[2KX: !mouseXpos! Y: !mouseYpos! ^| Click: !click!%\e%[11;4H%\e%[2KKeys pressed: !keysPressed!%\e%[12;4H%\e%[2KScrolled: !scrollpos!"
 )) < nul
